e2cda77c6aac2ea4307e231898f45443ac93c06e
danauerbach <dauerbach@ucsd.edu>
2024-09-16 21:05:59 +0000

    make sure /gsn mount point exists before mounting volume

--- mount-s3fgw-gsn.sh
+++ mount-s3fgw-gsn.sh
@@ -1,3 +1,4 @@
 #!/bin/sh 
 
-sudo mount -t nfs -o nolock,hard 10.2.4.94:/gsn-dcc-data-prod-us-east-2-74hfx024y /gsn
+/usr/bin/mkdir -p /gsn
+sudo /usr/bin/mount -t nfs -o nolock,hard 10.2.4.94:/gsn-dcc-data-prod-us-east-2-74hfx024y /gsn
